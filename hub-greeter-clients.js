2f5a1d5b791f402ec3f63884ba63abf72e8c4880
maltmann <michael.altmann@gmail.com>
2021-02-22 09:28:00 -0600

    Bookmarklet/TamperMonkey to filter out Spanish clients, clients that are in orgs that greeters should not handle or clients that are done via the virtual inbox

---
+++ hub-greeter-clients.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Show Images on GetYourRefund
+// @namespace    http://getyourrefund.org/
+// @version      0.2
+// @description  Show images on document pages.
+// @match        https://*.getyourrefund.org/en/hub/clients/
+// @grant        none
+// ==/UserScript==
+javascript: (function () {
+    var st = document.createElement('style');
+    st.innerHTML = `
+    .gyr-client-ignore {
+        color: pink !important;
+    }
+    `
+    document.getElementsByTagName('head')[0].appendChild(st);
+    function markClientsToIgnore() {
+        var orgsToSkip = ['Connecticut Association for Human Services',
+            'United Way of the Capital Region',
+            'United Way of Greater Cincinnati',
+            'United Way of Lancaster County',
+            'United Way of King County',
+            'Virtual VITA Inc'].map((s) => s.toLowerCase())
+        var formTableDataRows = document.querySelectorAll("table.client-table > tbody > tr")
+        formTableDataRows.forEach((row, index) => {
+            var tds = row.querySelectorAll("td");
+            var clientId = tds[1].innerText;
+            var org = tds[2].innerText;
+            var language = tds[3].innerText;
+            var returnListTd = tds[7];
+            var isVirtual = returnListTd.querySelectorAll('span.icon-move_to_inbox').length > 0;
+            // console.log(`${clientId} ${org} ${language} ${isVirtual}`)
+            if (language.toLowerCase().trim() === 'spanish' || isVirtual || orgsToSkip.includes(org.toLowerCase())) {
+                row.classList.add('gyr-client-ignore')
+            }
+        }
+        )
+    }
+    markClientsToIgnore();
+
+}())
