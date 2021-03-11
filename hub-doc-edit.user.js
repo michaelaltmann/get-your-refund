9cf6cf530f324e0de3be686a0dc2f2d9860a6028
plocket <52798256+plocket@users.noreply.github.com>
2021-03-11 08:51:39 -0500

    Log errors instead of throwing them

--- hub-doc-edit.user.js
+++ hub-doc-edit.user.js
@@ -8,28 +8,35 @@
 // @grant        none
 // ==/UserScript==
 javascript: (function () {
-    var st = document.createElement('style');
-    st.innerHTML = `
-    .gyr-preview {
-        width: 30%;
-        margin-left:20px;
-        display: inline-block
-    }
-    `
-    document.getElementsByTagName('head')[0].appendChild(st);
-    function addPreview() {
+    // Log errors instead of throwing them
+    try {
+        var st = document.createElement('style');
+        st.innerHTML = `
+        .gyr-preview {
+            width: 30%;
+            margin-left:20px;
+            display: inline-block
+        }
+        `
+        document.getElementsByTagName('head')[0].appendChild(st);
+        function addPreview() {
 
-        var slabs = document.querySelectorAll("section.slab")
-        slabs.forEach((slab, index) => {
-            var grid = slab.querySelector("div.grid")
-            if (grid) grid.style.display = 'inline-block';
-            var preview = document.createElement('img')
-            preview.className = 'gyr-preview'
-            var url = window.location.href.replace("/edit", "")
-            preview.src = url
-            slab.appendChild(preview)
-        })
-    }
-    addPreview();
-
+            var slabs = document.querySelectorAll("section.slab")
+            slabs.forEach((slab, index) => {
+                var grid = slab.querySelector("div.grid")
+                if (grid) grid.style.display = 'inline-block';
+                var preview = document.createElement('img')
+                preview.className = 'gyr-preview'
+                var url = window.location.href.replace("/edit", "")
+                preview.src = url
+                slab.appendChild(preview)
+            })
+        }
+        addPreview();
+    
+    // Avoid throwing errors so that other things, possibly other bookmarklets, can work on the page
+    } catch (err) {
+      // Do show the error to the developer, though
+      console.error( err );
+    }  // ends try/catch
 }())
