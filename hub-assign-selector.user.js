aa5cc6cf10489f9f74a96ec0ccc534d3e412013f
maltmann <michael.altmann@gmail.com>
2021-03-21 17:44:03 -0500

    Issue 12:  The assign selector has changed from a span to a div.

--- hub-assign-selector.user.js
+++ hub-assign-selector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Assign on the Hub
 // @namespace    http://getyourrefund.org/
-// @version      0.1
+// @version      0.2
 // @description  Easy to assign self on the Hub.
 // @match        https://*.getyourrefund.org/en/hub/clients*
 // @grant        none
@@ -9,15 +9,15 @@
 
 var currentUser = $('strong.user__name').text()
 
-$('span.tax-return-list__assignee').each(function(_,assignDiv) {
+$('div.tax-return-list__assignee').each(function (_, assignDiv) {
     var currentlyAssigned = $(assignDiv).find('a').text().trim() != "Assign";
     if (currentlyAssigned) {
         var assignedTo = $(assignDiv).find('a').text().trim();
         if (assignedTo === currentUser) {
             var unassignButton = $('<a>').text('Unassign').addClass('button button--small');
-            unassignButton.click(function() {
+            unassignButton.click(function () {
                 $(this).siblings('a')[0].click();
-                setTimeout(function() {
+                setTimeout(function () {
                     var select = $(assignDiv).find('select');
                     select.val(select.find('option:eq(0)').val());
                     $(assignDiv).find('button[type="submit"]')[0].click();
@@ -29,9 +29,9 @@ $('span.tax-return-list__assignee').each(function(_,assignDiv) {
     }
 
     var newButton = $('<a>').text('Assign to Me').addClass('button button--small')
-    newButton.click(function() {
+    newButton.click(function () {
         $(this).siblings('a')[0].click();
-        setTimeout(function() {
+        setTimeout(function () {
             var select = $(assignDiv).find('select');
             select.val(select.find(`option:contains("${currentUser}")`).val());
             $(assignDiv).find('button[type="submit"]')[0].click();
@@ -40,6 +40,4 @@ $('span.tax-return-list__assignee').each(function(_,assignDiv) {
     $(assignDiv).append(newButton);
 })
 
-$(document).on('mousedown', 'select[name="assigned_user_id"]', function(){
-    $(this).find(`option:contains("${currentUser}")`).insertBefore($(this).find('option:eq(1)'))
-});
+
